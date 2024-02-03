7968aef4b2620a22dd9d6ae342e76e5a4b20cd26
hammahutz <62766637+hammahutz@users.noreply.github.com>
2024-02-03 17:14:16 +0100

    Kontakt klar

--- src/components/Calendar/FormInput.jsx
+++ src/components/Calendar/FormInput.jsx
@@ -3,23 +3,46 @@ import { useState } from "react";
 const FormInput = ({ type, name }) => {
     const [value, setValue] = useState();
 
-    return (
-        <label class="form-control w-full max-w-xs">
-            <div class="label">
-                <span class="label-text">{name}</span>
-            </div>
-            <input
-                type={type}
-                class={`${type === "file" ? "file-" : ""}input ${type === "file" ? "file-" : ""}input-bordered w-full max-w-xs`}
-                placeholder={name}
-                name={name}
-                id={name}
-                value={value ? value : null}
-                onChange={(e) => setValue(e.target.value)}
-                required
-            />
-        </label>
-    );
+    if (type === "textarea") {
+        return (
+            <label class="form-control w-full max-w-xs">
+                <div class="label">
+                    <span class="label-text">{name}</span>
+                </div>
+                <textarea
+                    type={type}
+                    class="textarea textarea-bordered"
+                    placeholder={name}
+                    name={name}
+                    id={name}
+                    value={value ? value : null}
+                    onChange={(e) => setValue(e.target.value)}
+                    required
+                    rows="6"
+                />
+            </label>
+        );
+    } else {
+        return (
+            <label class="form-control w-full max-w-xs">
+                <div class="label">
+                    <span class="label-text">{name}</span>
+                </div>
+                <input
+                    type={type}
+                    class={`${type === "file" ? "file-" : ""}input ${
+                        type === "file" ? "file-" : ""
+                    }input-bordered w-full max-w-xs`}
+                    placeholder={name}
+                    name={name}
+                    id={name}
+                    value={value ? value : null}
+                    onChange={(e) => setValue(e.target.value)}
+                    required
+                />
+            </label>
+        );
+    }
 };
 
 export default FormInput;
