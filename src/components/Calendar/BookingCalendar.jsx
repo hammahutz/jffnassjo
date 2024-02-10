571bef024d923e2f74e620075954cceb2b5e87a5
hammahutz <62766637+hammahutz@users.noreply.github.com>
2024-02-10 10:54:21 +0100

    Fungerande kalender

--- src/components/Calendar/BookingCalendar.jsx
+++ src/components/Calendar/BookingCalendar.jsx
@@ -1,56 +1,62 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import FormDisplay from "./FormDisplay";
+import "react-calendar/dist/Calendar.css";
+// import styles from "./BookingCalendar.module.css";
 
-import Calendar from 'react-calendar';
-import FormDisplay from './FormDisplay';
-import 'react-calendar/dist/Calendar.css';
-import styles from './BookingCalendar.module.css'
+import data from "../../data.json";
 
-
-
-const BookingCalendar = ({ maxDaysPerBooking, pricePerDay }) => {
-
-
+const BookingCalendar = () => {
+    const { maxAntalDagar, prisPerDag } = data.pallarpUthyrning;
     const [date, setDate] = useState([new Date(), new Date()]);
-    const [cost, setCost] = useState(pricePerDay);
-    const [maxDate, setMaxDate] = useState();
+    const [cost, setCost] = useState(prisPerDag);
+    const [maxDate, setMaxDate] = useState(null);
     const [minDate, setMinDate] = useState(new Date());
 
-    const onChange = date => {
-        setDate(date);
-        setCost(
-            (Math.abs(date[1].getDate() - date[0].getDate()) + 1) * pricePerDay
-        );
-    }
+    const onChange = (date) => {
+        if (date[1] === undefined) {
+            setDate([date, date]);
+        } else {
+            setDate(date);
+        }
+    };
+
     const onClickDay = (value, event) => {
+        if (maxAntalDagar <= 1) {
+            return;
+        }
         if (maxDate == undefined) {
             setBookingRange(value);
-        }
-        else {
+        } else {
             resetBookingRange();
         }
+    };
 
-    }
+    useEffect(() => {
+        console.log(date);
+        setCost((Math.abs(date[1].getDate() - date[0].getDate()) + 1) * prisPerDag);
+    }, [date]);
 
     const setBookingRange = (startDate) => {
         const maxBookingDate = new Date(startDate);
-        maxBookingDate.setDate(startDate.getDate() + parseInt(maxDaysPerBooking) - 1);
+        maxBookingDate.setDate(startDate.getDate() + parseInt(maxAntalDagar) - 1);
         setMaxDate(maxBookingDate);
         setMinDate(startDate);
-    }
+    };
     const resetBookingRange = () => {
         setMaxDate(undefined);
         setMinDate(new Date());
-    }
+    };
 
     return (
         <>
-            <div class="row justify-content-center align-items-center mb-3">
+            <div className="row justify-content-center align-items-center mb-3">
                 <div className="col center">
                     <Calendar
-                        className={[styles.calendar, "form-control"]}
+                        className={["form-control"]}
                         onChange={onChange}
                         value={date}
-                        selectRange
+                        selectRange={maxAntalDagar > 1}
                         showWeekNumbers
                         onClickDay={onClickDay}
                         maxDate={maxDate}
@@ -58,13 +64,25 @@ const BookingCalendar = ({ maxDaysPerBooking, pricePerDay }) => {
                     />
                 </div>
                 <div className="col">
-                    <FormDisplay type="text" name="Kostnad" value={`${cost} kr`} />
-                    <FormDisplay type="date" name="Från" value={date[0]?.toLocaleDateString()} />
-                    <FormDisplay type="date" name="Till" value={date[1]?.toLocaleDateString()} />
+                    <FormDisplay
+                        type="text"
+                        name="Pris"
+                        value={`${cost} kr`}
+                    />
+                    <FormDisplay
+                        type="date"
+                        name="Från"
+                        value={date[0]?.toLocaleDateString()}
+                    />
+                    <FormDisplay
+                        type="date"
+                        name="Till"
+                        value={date[1]?.toLocaleDateString()}
+                    />
                 </div>
             </div>
         </>
     );
-}
+};
 
 export default BookingCalendar;
--- src/components/Calendar/FormInput.jsx
+++ src/components/Calendar/FormInput.jsx
@@ -1,42 +1,56 @@
 import { useState } from "react";
+import BookingCalendar from "./BookingCalendar";
 
 const FormInput = ({ type, name }) => {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState("");
+
+    const onValueChange = (e) => {
+        setValue(e.target.value);
+    };
 
     if (type === "textarea") {
         return (
-            <label class="form-control w-full max-w-xs">
-                <div class="label">
-                    <span class="label-text">{name}</span>
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">{name}</span>
                 </div>
                 <textarea
                     type={type}
-                    class="textarea textarea-bordered placeholder:italic"
+                    className="textarea textarea-bordered placeholder:italic"
                     placeholder={name}
                     name={name}
                     id={name}
-                    value={value ? value : null}
-                    onChange={(e) => setValue(e.target.value)}
+                    value={value}
+                    onChange={(e) => onValueChange(e)}
                     required
                     rows="6"
                 />
             </label>
         );
+    } else if (type === "calendar") {
+        return (
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">{name}</span>
+                </div>
+                <BookingCalendar client:load />
+            </label>
+        );
     } else {
         return (
-            <label class="form-control w-full max-w-xs">
-                <div class="label">
-                    <span class="label-text">{name}</span>
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">{name}</span>
                 </div>
                 <input
                     type={type}
-                    class={`${type === "file" ? "file-" : ""}input ${
+                    className={`${type === "file" ? "file-" : ""}input ${
                         type === "file" ? "file-" : ""
                     }input-bordered w-full max-w-xs placeholder:italic`}
                     placeholder={name}
                     name={name}
                     id={name}
-                    value={value ? value : null}
+                    value={value}
                     onChange={(e) => setValue(e.target.value)}
                     required
                 />
